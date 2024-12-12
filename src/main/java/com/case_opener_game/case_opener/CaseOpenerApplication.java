f41fdac78f18940bbb0b5e37213d8cc52a0c9e5e
vladislavDunin <vladislavdunin.java@gmail.com>
2024-12-12 20:35:19 +0300

    init commit

---
+++ src/main/java/com/case_opener_game/case_opener/CaseOpenerApplication.java
@@ -0,0 +1,15 @@
+package com.case_opener_game.case_opener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseOpenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseOpenerApplication.class, args);
+
+		System.out.println(123);
+	}
+
+}
---
+++ src/test/java/com/case_opener_game/case_opener/CaseOpenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case_opener_game.case_opener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseOpenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
